6f7e2998936c4e60a37a89df73cf725304175b4f
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-17 14:17:16 -0300

    Initial commit

---
+++ js/segregacao.js
@@ -0,0 +1,10 @@
+console.log('Olá Mundo, introdução a JS.');
+console.log(`Data hora: ${new Date()}`);
+
+function mensagem() {
+    alert('Chamamos um modal');
+}
+
+function mensagem2() {
+    alert('Mensagem2');
+}
