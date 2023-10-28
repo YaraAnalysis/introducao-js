01d5b7ef6ed14f3a2ec1d97bfdc5954a2f6b6bd5
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-28 00:29:46 -0300

    Funções

--- js/segregacao.js
+++ js/segregacao.js
@@ -1,13 +1,22 @@
-const olaMundo = 'Olá Mundo, intridução a JS.';
+const olaMundo = 'Olá Mundo, introdução a JS.';
 const dataAtual = new Date();
 
 console.log(olaMundo);
 console.log(`Data hora: ${dataAtual}`);
 
-function mensagem() {
-    alert(olaMundo);
+function mensagem(mensagem) {
+    alert(mensagem);
 }
 
 function mensagem2() {
-    alert(olaMundo);
+    mensagem('Aprendendo funções');
 }
+
+const mensagemFinal = (mensagemFinal) =>{
+    console.log(mensagemFinal);
+}
+
+mensagemFinal('Concluímos a execução');
+mensagemFinal('Segunda mensagem');
+mensagemFinal('Terceira mensagem');
+mensagemFinal('Quarta mensagem');
