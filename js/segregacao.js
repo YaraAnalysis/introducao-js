99072931716283e7d008384ae21beee62c152060
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-28 00:57:26 -0300

    Operadores

--- js/segregacao.js
+++ js/segregacao.js
@@ -16,7 +16,17 @@ const mensagemFinal = (mensagemFinal) =>{
     console.log(mensagemFinal);
 }
 
-mensagemFinal('Concluímos a execução');
-mensagemFinal('Segunda mensagem');
-mensagemFinal('Terceira mensagem');
-mensagemFinal('Quarta mensagem');
+const verdadeiro = true;
+const naoVerdadeiro = !verdadeiro;
+
+const tudoEVerdadeiro = verdadeiro && naoVerdadeiro;
+const tudoEFalso = !verdadeiro && naoVerdadeiro;
+const algumEVerdadeiro = verdadeiro || naoVerdadeiro;
+const algumEFalso = verdadeiro || naoVerdadeiro
+
+console.log('verdadeiro', verdadeiro);
+console.log('naoVerdadeiro', naoVerdadeiro);
+console.log('tudoEVerdadeiro', tudoEVerdadeiro);
+console.log('tudoEFalso', tudoEFalso);
+console.log('algumEVerdadeiro', algumEVerdadeiro);
+console.log('algumEFalso', algumEFalso);
