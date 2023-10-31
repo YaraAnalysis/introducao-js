dfef7d957e5cbb40781748b057f3f6bfa80e94bb
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-10-30 23:33:44 -0300

    Operadores

---
+++ js/operadores.js
@@ -0,0 +1,11 @@
+
+const numero1 = 32;
+const numero2 = 24;
+const numero3 = 14;
+const numero4 = 1;
+const stringNumero = '1';
+
+console.log('é igual em valor', numero4 == stringNumero);
+console.log('é igual em valor e tipo', numero4 === stringNumero);
+console.log('é diferente em valor', numero4 != stringNumero);
+console.log('é diferente em valor e tipo', numero4 !== stringNumero);
