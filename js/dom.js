91becdfaca395ff18f65fe725463edcda8b805bd
Yara Vasconcellos <114318856+YaraAnalysis@users.noreply.github.com>
2023-11-16 22:41:02 -0300

    Utilizando jQuery

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,27 @@
-const divs = document.getElementsByTagName('div');
-const h2s = document.getElementsByTagName('h2');
-const titulo = document.getElementById('titulo');
-const paragrafo = document.getElementById('paragrafo');
-const input = document.getElementById('input');
-const elementsMarginTop2 = document.getElementsByClassName('mt-2');
+//const divs = document.getElementsByTagName('div');
+const divs = $('div');
+//const h2s = document.getElementsByTagName('h2');
+const h2s = $('h2');
+//const titulo = document.getElementById('titulo');
+const titulo = $('#titulo');
+//const paragrafo = document.getElementById('paragrafo');
+const paragrafo = $('#paragrafo');
+//const input = document.getElementById('input');
+const input = $('input');
+//const elementsMarginTop2 = document.getElementsByClassName('mt-2');
+const elementsMarginTop2 = $('.mt-2');
 
 function alterarHtml(){
-    titulo.textContent = input.value;
-    titulo.style.color = '#ac4389';
+    titulo.text(input.val());
+    titulo.css('color', '#ac4389');
     for(let element of elementsMarginTop2){
-        element.className = 'mt-4';
+        $(element).addClass('mt-4');
     }
 
-    input.value = '';
+    input.val('');
 
     const novoP = document.createElement('p');
     novoP.textContent = 'Novo texto adicionado via JavaScript.'
 
-    paragrafo.parentNode.appendChild(novoP);
+    paragrafo.after(novoP);
 }
